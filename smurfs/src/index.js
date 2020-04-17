51d0e2cbd5b7eb064ed33a75bc7c03cba8418a0d
Matthew Kearney <matthewkearney@Matthews-MBP.attlocal.net>
2020-04-17 11:37:20 -0500

    code broken adding answers

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,21 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
-import thunk from "redux-thunk";
-import reducer from "./reducers/reducer.js";
-import { applyMiddleware, createStore } from 'redux';
-import { Provider } from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import {Provider} from 'react-redux';
+import Reducer from './reducer/reducer'
 
 
-
-const store = createStore ( reducer, applyMiddleware(thunk)
-);
-
+const store = createStore(Reducer, applyMiddleware(thunk));
 ReactDOM.render(
-    <Provider store={store}>
-        <App/>
-    </Provider>
-, document.getElementById('root'));
-
-
-
+<Provider store={store}>
+    <App />
+</Provider>, 
+document.getElementById("root"));
